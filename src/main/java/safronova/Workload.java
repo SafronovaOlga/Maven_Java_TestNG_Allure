c083b61a95e99edca1aab29a9a965604653bd0f7
SafronovaOlga <104982522+SafronovaOlga@users.noreply.github.com>
2025-03-16 11:15:41 +0100

    Created new classes and updated POM file (#5)

---
+++ src/main/java/safronova/Workload.java
@@ -0,0 +1,1 @@
+package safronova;public class Workload {        public static final double VERY_HIGH_WORKLOAD = 1.6;    public static final double HIGH_WORKLOAD = 1.4;    public static final double NORMAL_WORKLOAD = 1.2;    public static final double LOW_WORKLOAD = 1;}
